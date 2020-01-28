08ae590ad25322ba080a80d992d0a6b4f83f119c
Saul Alvarez <saul@Sauls-MacBook.local>
2020-01-28 12:35:11 -0800

    brew.sh created for running brew scripts

---
+++ brew.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Install Brew Packages
+brew install bash
+brew install git
+brew install python
+brew install tree
+
+# install applications with Cask
+brew cask install slack
+brew cask install google-chrome
+brew cask install sublime-text
+brew cask install atom
+brew cask install hyperdock
+brew cask install google-backup-and-sync
+
+#### FIND OUT THE CORRECT FONTS TO INSTALL!
+# Install Source Code Pro Font
+brew tap homebrew/cask-fonts
+brew cask install font-source-code-pro
