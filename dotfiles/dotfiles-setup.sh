b90ac59f3d0c4501e6561f7185e45602d0a80d9c
Saul Alvarez <saul@sauls-macbook.gateway.sonic.net>
2020-01-28 20:37:59 -0800

    script auto-creates dotfiles

--- dotfiles/dotfiles-setup.sh
+++ dotfiles/dotfiles-setup.sh
@@ -1,1 +1,20 @@
 #!/usr/bin/env bash
+
+############################
+# This script creates symlinks from the home directory to any desired dotfiles in ${homedir}/dotfiles
+
+############################
+
+# files="exports bashrc aliases inputrc selected_editor vimrc"
+files="exports bashrc aliases inputrc selected_editor vimrc"
+
+
+DOTFILESDIR=$HOME/Documents/GitHub/Fresh-Install/dotfiles
+echo $DOTFILESDIR
+
+cd $DOTFILESDIR
+
+for file in ${files}; do
+    echo "Creating symlink to $file in home directory."
+    ln -sf $DOTFILESDIR/.$file $HOME/.$file
+done
