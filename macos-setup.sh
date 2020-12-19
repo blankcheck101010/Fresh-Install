9697ba560880d43959a3822d27b7530e1ed5ce96
Saul Alvarez <saul@Sauls-MacBook.local>
2020-12-19 02:44:35 -0800

    updated order and added comments to macos-setup.sh

--- macos-setup.sh
+++ macos-setup.sh
@@ -2,15 +2,8 @@
 
 # Run this bash script manually. 'bash macos-setup.sh'
 
-# Change the shell back to bash (Starting Catalina, zsh is default shell)
-chsh -s /bin/bash
-
 # Install homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-### below are install with Brew Cask ###
-chmod +x $HOME/Documents/GitHub/Fresh-Install/brew.sh
-$HOME/Documents/GitHub/Fresh-Install/brew.sh
 
 # install was (Mac Apple Store Command Line Tool)
 brew install mas
@@ -18,8 +11,10 @@ brew install mas
 mas signin blankcheck101010@yahoo.com
 
 ### Below are installed by Mac Apple Store only ###
-# install Xcode
+# install Xcode (This is required to run git commands with Terminal)
 mas install 497799835
+sudo xcode-select --reset
+sudo xcodebuild -license
 
 # install todoist 
 mas install 585829637
@@ -32,6 +27,7 @@ defaults write "com.todoist.mac.Todoist" ShowTodoistMenuBar -bool false
 # setup Fresh-Install Repo and Dotfiles
 mkdir ~/Documents/GitHub
 cd ~/Documents/GitHub
+# Git requires xcode
 git clone https://github.com/blankcheck101010/Fresh-Install.git
 cd ~/Documents/GitHub/Fresh-Install
 git branch develop
@@ -40,9 +36,20 @@ git fetch --all
 git reset --hard origin/develop  # this resolves any conflicts with master
 git pull origin develop
 git branch --set-upstream-to origin/develop
-# Dofiles Setup
+
+
+### below are install with Brew Cask ###
+chmod +x $HOME/Documents/GitHub/Fresh-Install/brew.sh
+$HOME/Documents/GitHub/Fresh-Install/brew.sh
+
+# Change the shell back to bash (Starting Catalina, zsh is default shell)
+# Must install bash first to get updated filepath (/usr/local/bin/bash) used below
+chsh -s /usr/local/bin/bash
+
+# Dofiles Setup (requires bash to work correctly)
 chmod +x $HOME/Documents/GitHub/Fresh-Install/dotfiles/dotfiles-setup.sh
 $HOME/Documents/GitHub/Fresh-Install/dotfiles/dotfiles-setup.sh
+
 
 # setup Sublime Repo
 cd ~/Documents/GitHub
